13615227718d7d595b64897e01f283af28e70f40
Lucas Fernando <ferlucas074@gmail.com>
2026-02-22 00:18:09 -0300

    feat: implementa formulário de cadastro com nome, email, senha e botão de criação de conta

--- app/src/main/java/br/com/fiap/recipes/screens/SignupScreen.kt
+++ app/src/main/java/br/com/fiap/recipes/screens/SignupScreen.kt
@@ -1,15 +1,37 @@
 package br.com.fiap.recipes.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.RemoveRedEye
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.fiap.recipes.R
 import br.com.fiap.recipes.ui.theme.RecipesTheme
 
@@ -54,5 +76,163 @@ private fun TitleComponentPreview() {
 
 @Composable
 fun UserImage(modifier: Modifier = Modifier) {
-    
+    Box(
+        modifier = Modifier
+            .size(120.dp)
+    ){
+        Image(
+            painter = painterResource(R.drawable.user),
+            contentDescription = stringResource(R.string.user_image),
+            modifier = Modifier
+                .size(110.dp)
+                .align(alignment = Alignment.Center)
+        )
+        Icon(
+            imageVector = Icons.Default.PhotoCamera,
+            contentDescription = stringResource(R.string.camera_icon), // TRADUZIR PARA OUTROS IDIOMAS
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .align(alignment = Alignment.BottomEnd)
+
+        )
+    }
 }
+
+@Preview(
+    showBackground = true
+)
+@Composable
+private fun UserImagePreview() {
+    RecipesTheme {
+        UserImage()
+    }
+}
+
+@Composable
+fun SignupUserForm(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        //  CAIXA DE TEXTO PARA NOME DO USUÁRIO
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(
+                    text = stringResource(R.string.your_name),
+                    style = MaterialTheme.typography.labelSmall
+                ) // TRADUZIR PARA OUTROS IDIOMAS
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = stringResource(R.string.person_icon), // TRADUZIR PARA OUTROS IDIOMAS
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        )
+        //  CAIXA DE TEXTO PARA E-MAIL DO USUÁRIO
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(
+                    text = stringResource(R.string.your_e_mail), // TRADUZIR PARA OUTROS IDIOMAS
+                    style = MaterialTheme.typography.labelSmall
+                )
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Mail,
+                    contentDescription = stringResource(R.string.email_icon), // TRADUZIR PARA OUTROS IDIOMAS
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        )
+        //  CAIXA DE TEXTO PARA SENHA DO USUÁRIO
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(
+                    text = stringResource(R.string.your_password), // TRADUZIR PARA OUTROS IDIOMAS
+                    style = MaterialTheme.typography.labelSmall
+                ) // TRADUZIR PARA OUTROS IDIOMAS
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = stringResource(R.string.password_icon), // TRADUZIR PARA OUTROS IDIOMAS
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.RemoveRedEye,
+                    contentDescription = stringResource(R.string.remove_red_eye_icon), // TRADUZIR PARA OUTROS IDIOMAS
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        )
+        // BOTÃO CREATE ACCOUNT
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.create_a_new_account),
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+private fun SignupUserFormPreview() {
+    RecipesTheme {
+        SignupUserForm()
+    }
+}
+
+
+
+
+
+
+
+
+
