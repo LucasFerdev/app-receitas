6b047ddbb51ffbdccd30b8bda315741dc02746e8
Lucas Fernando <ferlucas074@gmail.com>
2026-02-27 23:01:57 -0300

    cria model Category e repository com dados mockados

---
+++ app/src/main/java/br/com/fiap/recipes/components/CategoryItem.kt
@@ -0,0 +1,9 @@
+package br.com.fiap.recipes.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun (modifier: Modifier = Modifier) {
+    
+}
